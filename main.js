de7c030d7d0393fea99686b169ef906bee316742
pjnalls <42269808+pjnalls@users.noreply.github.com>
2025-06-21 09:32:44 -0500

    feat(nav): add a navbar component

---
+++ main.js
@@ -0,0 +1,9 @@
+(function () {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('navLinks');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+})();
